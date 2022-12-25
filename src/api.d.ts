7b360002b3c811f2182537873d9ce393f4425f6e
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2022-12-25 15:18:31 +0200

    fix(typings): export ALPNProtocol as "const enum" (#332)

--- src/api.d.ts
+++ src/api.d.ts
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-export enum ALPNProtocol {
+export const enum ALPNProtocol {
   ALPN_HTTP2 = 'h2',
   ALPN_HTTP2C = 'h2c',
   ALPN_HTTP1_1 = 'http/1.1',
